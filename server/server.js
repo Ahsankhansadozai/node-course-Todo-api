72f682757cfb08337893d29507e3eb2dcefa3b0f
Ahsan <ahsan3200@iiu.edu.pk>
2019-08-02 04:14:28 -0700

    ADD PATCH/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var { ObjectID } = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { User } = require("./models/User");
@@ -73,6 +74,30 @@ app.delete("/todos/:id", (req, res) => {
     };
 });
 
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        res.status(400).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
